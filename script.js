2b14dd33babe1f1b34f02730703c29d066057bd7
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-02-01 10:56:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const buttons = document.querySelectorAll('button')
 const validInputs = "00123456789/*+-."
 let result = '';
 
-DisplayBar.disabled = true;
-DisplayBar.value = ''
 
 //1. Add the values in search bar by clicking
 buttons.forEach(button => {
@@ -12,9 +10,10 @@ buttons.forEach(button => {
         
         const num = e.target.innerHTML;
         const text = DisplayBar.value;
+        console.log(text);
         
         // can't click equals to on empty input box
-        if(text == '' && num == '='){
+        if(text == '' && num == '=' ){
             //do nothing
         }
         // if it is a validInput -> add to displaybar
@@ -30,23 +29,11 @@ buttons.forEach(button => {
         else if(num == 'DEL'){
             result = DisplayBar.value.slice(0,-1);
         }
-        else if(num == '=' || num == 'Enter'){
+        else if(num == '='){
             result = (eval(result));
         }
         DisplayBar.value = (result);    
 
-        saveData();
  
     })
 });
-
-// all the data inside DisplayBar will be saved as name = "data" in local storage
-function saveData() {
-    localStorage.setItem("data", DisplayBar.value);
-}
-
-// display saved data everytime we reload
-function showData(){
-    DisplayBar.value = localStorage.getItem("data");
-}
-showData();
